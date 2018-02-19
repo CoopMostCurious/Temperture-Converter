34324d3242402becce9c9a9354bf2424f709b807
Cooper Hanke <hanke31314@gmail.com>
2018-02-19 01:33:10 -0600

    design and project complete for now, as far as barebones

--- app/app.js
+++ app/app.js
@@ -1,32 +1,35 @@
 (function() {
     'use strict';
-    const celciusInput = document.querySelector('#celcius > input');
-    const fahInput = document.querySelector('#fahrenheit > input');
-    const kelvinInput = document.querySelector('#kelvin > input');
+    var celciusInput = document.querySelector('#celcius > input');
+    var fahInput = document.querySelector('#fahrenheit > input');
+    var kelvinInput = document.querySelector('#kelvin > input');
+
+    function roundNumbers(number) {
+        return Math.round(number*100)/100;
+    }
 
     function updateCelcius() {
         var cTemp = parseFloat(celciusInput.value);
         var fTemp = (cTemp * (9/5)) + 32;
         var kTemp = cTemp + 273.15;
-        fahInput.value = fTemp;
-        kelvinInput.value = kTemp;
+        fahInput.value = roundNumbers(fTemp);
+        kelvinInput.value = roundNumbers(kTemp);
     }
 
     function updateFahrenheit() {
         var fTemp = parseFloat(fahInput.value);
         var cTemp = (fTemp - 32) * (5/9);
         var kTemp = fTemp + 459.67 * 5/9;
-        celciusInput.value = cTemp;
-        kelvinInput.value = kTemp;
+        celciusInput.value = roundNumbers(cTemp);
+        kelvinInput.value = roundNumbers(kTemp);
     }
 
     function updateKelvin() {
         var kTemp = parseFloat(kelvinInput.value);
         var cTemp = kTemp - 273.15;
         var fTemp = 9/5 * (kTemp - 273.15) + 32;
-        celciusInput.value = cTemp;
-        console.log(celciusInput.value);
-        fahInput.value = fTemp;
+        celciusInput.value = roundNumbers(cTemp);
+        fahInput.value = roundNumbers(fTemp);
     }
 
     celciusInput.addEventListener('input', updateCelcius);
